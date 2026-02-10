bc37166f9a8a234b70a7ab474fedbc4e0166acbb
formifan2002 <d.behrendt@web.de>
2026-02-10 19:26:47 +0100

    Fix build1.yml

--- cmake/PluginPackage.cmake
+++ cmake/PluginPackage.cmake
@@ -35,14 +35,15 @@ if (OCPN_FLATPAK_CONFIG)
   add_custom_command(
       TARGET flatpak-pkg
       POST_BUILD
-      COMMAND echo "DEBUG: Packaging from ${CMAKE_CURRENT_BINARY_DIR}/app/files"
+      COMMAND ${CMAKE_COMMAND} -E echo "DEBUG: Packaging from ${CMAKE_CURRENT_BINARY_DIR}/app/files"
       COMMAND ${TAR} -czf
               ${PKG_NVR}-${ARCH}${PKG_TARGET_WX_VER}_${PKG_TARGET_NVR}.tar.gz
               --verbose
-              --transform=s|.*/files/|${PACKAGE}-flatpak-${PACKAGE_VERSION}/|
+              --transform=s,.*/files/,${PACKAGE}-flatpak-${PACKAGE_VERSION}/,
               ${CMAKE_CURRENT_BINARY_DIR}/app/files
       COMMAND chmod -R a+wr ../build
   )
+
 
   message(
     STATUS
