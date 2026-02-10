f4525289f184e049cac1c9106eaa744ce03b90a1
formifan2002 <d.behrendt@web.de>
2026-02-10 18:38:16 +0100

    Fix build1.yml

--- cmake/PluginPackage.cmake
+++ cmake/PluginPackage.cmake
@@ -29,26 +29,20 @@ if (OCPN_FLATPAK_CONFIG)
       /usr/bin/flatpak-builder --force-clean -v ${CMAKE_CURRENT_BINARY_DIR}/app
       ${CMAKE_CURRENT_BINARY_DIR}/flatpak/org.opencpn.OpenCPN.Plugin.${PACKAGE}.yaml
   )
-  
-  add_custom_target("flatpak-pkg")
 
+  add_custom_target("flatpak-pkg")
+  
   add_custom_command(
       TARGET flatpak-pkg
       POST_BUILD
-      COMMAND sh -c "
-          FILES_DIR=\$(find ${CMAKE_CURRENT_BINARY_DIR}/flatpak/.flatpak-builder/rofiles -type d -name files | head -n 1) && \
-          if [ -z \"\$FILES_DIR\" ]; then
-              echo 'ERROR: Could not locate Flatpak output directory (files/)'; exit 1;
-          fi && \
-          ${TAR} -czf \
-              ${PKG_NVR}-${ARCH}${PKG_TARGET_WX_VER}_${PKG_TARGET_NVR}.tar.gz \
-              --verbose \
-              --transform=s|.*/files/|${PACKAGE}-flatpak-${PACKAGE_VERSION}/| \
-              \"\$FILES_DIR\"
-      "
+      COMMAND sh -c "FILES_DIR=\$(find ${CMAKE_CURRENT_BINARY_DIR}/flatpak/.flatpak-builder/rofiles -type d -name files | head -n 1); \
+                     if [ -z \"\$FILES_DIR\" ]; then echo 'ERROR: Could not locate Flatpak output directory (files/)'; exit 1; fi; \
+                     ${TAR} -czf ${PKG_NVR}-${ARCH}${PKG_TARGET_WX_VER}_${PKG_TARGET_NVR}.tar.gz \
+                     --verbose \
+                     --transform=s|.*/files/|${PACKAGE}-flatpak-${PACKAGE_VERSION}/| \
+                     \"\$FILES_DIR\""
       COMMAND chmod -R a+wr ../build
   )
-
 
   message(
     STATUS
